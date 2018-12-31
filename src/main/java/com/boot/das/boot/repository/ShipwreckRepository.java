0b5a1489f2d6962f34d8c7c64f9e0486a9bf1ae1
danhillcode <danhilltravel@hotmail.co.uk>
2018-12-31 17:36:14 +0000

    added functionality for the rest API to shipwreckcontroller and repository file  however some config error on start up

---
+++ src/main/java/com/boot/das/boot/repository/ShipwreckRepository.java
@@ -0,0 +1,13 @@
+package com.boot.das.boot.repository;
+
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.boot.das.boot.model.Shipwreck;
+
+public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
+
+}
+
